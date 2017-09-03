58f5d0eddb23513c25936e0dec6c6fec00eae916
liidian <863397251+liidian@users.noreply.github.com>
2017-09-03 16:05:48 -0400

    homework practice 0903

--- Sesion 1/cal.py
+++ Sesion 1/cal.py
@@ -1,1 +1,22 @@
 print(46+37+38)
+
+print((42*60+42)/42, 'number/42sec')
+
+print(10/1.61, 'miles')
+
+print(10/1.61/(42+42/60), 'miles/minute')
+
+print(10/1.61/(42*60+42), 'miles/second')
+
+print(10/1.61/(42+42/60)*60, 'miles/hour') #this is testing 1, 2 , 3
+
+
+pi=3.14
+r=5
+Volume=4/3*pi*(r**3)
+Volume
+
+print('$', 24.95*0.6+3+0.75*59)
+
+print((8+15/60)*2+(7+12/60)*3)+(6+52/60))
+
--- Sesion 1/hello.py
+++ Sesion 1/hello.py
@@ -1,3 +1,32 @@
-print('hello,world')
+print('hello,Zhi')
+print(2**6)
+print(2016-23)
+print(15/3)
+print(2, 912)
 
-print('abc')
+
+
+
+
+print('whats your name', input())
+
+message = 'I did something cool today!'
+print(message)
+
+n=100
+
+pi = 3.14
+print(n)
+
+first_name = 'CC'
+Last_name = 'Huang'
+
+print (first_name + Last_Name*10)
+
+
+'today is %2d-%02d.' %(9,6)
+'pi=%.2f.' %3.1415926
+'Growth rate: %d %%' % 7
+
+
+
